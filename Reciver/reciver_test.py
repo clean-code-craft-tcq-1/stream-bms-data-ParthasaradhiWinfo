cbab8a56286a1a0df5d8ddca64376603de875831
VinothRaghu <77480559+VinothRaghu@users.noreply.github.com>
2021-06-26 19:34:51 +0530

    Update reciver_test.py
    Completing test and project by adding test for more than and less than 5 inputs

--- Reciver/reciver_test.py
+++ Reciver/reciver_test.py
@@ -1,45 +1,41 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Jun 24 02:28:15 2021
-
 @author: VNO1COB
 """
 import unittest
 import input_processing
 
 
-class test_sender_max_min_avf(unittest.TestCase):
+class test_sender_max_min_avg(unittest.TestCase):
     
-   def test_avg_five_val(self):
-      self.assertTrue(len(input_processing.averagecalculation([88.33 , 99, 65, 195.4, 141.6])),3)
+   def test_mininmum_value(self):
+      minimum_value=input_processing.average_of_input_list([94.95 , 96, 112, 100.4, 97.6])[0]
+      print(minimum_value)
+      self.assertEqual(minimum_value,94.95) 
 
-   def test_min(self):
-      self.assertTrue(input_processing.averagecalculation([94.95 , 96, 111, 100.4, 97.6])[0],111) 
+   def test_maximum_value(self):
+      maximum_value=input_processing.average_of_input_list([88.33 , 99, 65, 195.4, 141.6])[1]
+      print(maximum_value)
+      self.assertEqual(maximum_value,195.4)
+   def test_average_value(self):
+      average_store=input_processing.average_of_input_list([88.33 , 99, 65, 195.4, 141.6])[2]
+      print(average_store)
+      self.assertEqual(average_store,117.86600000000001)
 
-   def test_max(self):
-      self.assertTrue(input_processing.averagecalculation([197.204 , 392, 10, 10.4, 33.6])[1],205) 
-
-   def test_avg(self):
-      self.assertTrue(input_processing.averagecalculation([30,30,30,10,40])[2],10)
-    
-   def test_empty_input(self):
-      with self.assertRaises(ValueError) as context:
-            input_processing.classify_input(" ")
-      self.assertTrue('wrong_input_data' in str(context.exception))
-
-            input_processing.classify_input("102 CR")
-      self.assertTrue('wrong_input_data' in str(context.exception))
+   
       
-   def test_single_input(self):
-      with self.assertRaises(ValueError) as context:
-            input_processing.classify_input("205.7")
-      self.assertTrue('wrong_input_data' in str(context.exception))  
-
-   def test_avg_less_val(self):
-      self.assertTrue(len(input_processing.averagecalculation([133.33 , 144, 111])),2)    
-      
+   def test_avg_val_for_more_than_six_input(self):
+            average_last_five_value=input_processing.average_of_input_list([88.33 , 99, 65, 195.4,140.0,140.0,106.56666666666666])[2]
+            print(average_last_five_value)
+            self.assertEqual(average_last_five_value,92.42380952380952) 
+  
+   def test_avg_val_for_less_than_five_input(self):
+            average_less_than_five_value=input_processing.average_of_input_list([88.33 , 99, 65, 195.4])
+            print(average_less_than_five_value)
+            self.assertEqual(average_less_than_five_value,[65, 195.4]) 
   
           
 
 #if __name__ == '__main__':
- # unittest.main()
+# unittest.main()
